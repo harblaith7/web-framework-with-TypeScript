48b931a48cd7e9aea1bdd8c4dbaa43b412e5e6fb
Laith Harb <harblaith77@gmail.com>
2020-04-15 11:40:00 -0400

    Added set random age functionality

--- src/Views/UserForm.ts
+++ src/Views/UserForm.ts
@@ -1,27 +1,35 @@
+import {User} from "../models/User"
+
 export class UserForm {
-    constructor(public parent: Element){}
+    constructor(public parent: Element, public model: User){
+        this.bindModel()
+    }
+
+    bindModel(): void {
+        this.model.on("change", () => {
+            this.render()
+        })
+    }
     
     eventsMap(): {[key: string] : () => void} {
         return {
-            "click:button"  : this.onButtonClick,
-            "mouseenter:h1" : this.onHeaderHover
+            "click:.set-age": this.onSetAgeClick
         }
     }
 
-    onButtonClick(): void {
-        console.log("Hi there")
-    }
-
-    onHeaderHover(): void {
-        console.log("H1 was hovered over")
+    onSetAgeClick = (): void => {
+        this.model.setRandomAge()
     }
 
     template(): string {
         return `
             <div>
                 <h1> User form </h1>
+                <div>User name: ${this.model.get("name")}</div>
+                <div>User age: ${this.model.get("age")}</div>
                 <input/>
                 <button>Click Me </button>
+                <button class="set-age">Set Random Age </button>
             <div>
         `
     }
@@ -39,6 +47,9 @@ export class UserForm {
     }
 
     render(): void {
+
+        this.parent.innerHTML = "";
+
         const templateElement = document.createElement("template");
         templateElement.innerHTML = this.template()
 
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,12 @@
 import {UserForm} from "./Views/UserForm"
+import {User} from "./Models/User"
+
+const user = User.buildUser({name: "Katie", age: 32})
 
 
 const userForm = new UserForm(
-    document.getElementById("root")
+    document.getElementById("root"),
+    user
 )
 
 
